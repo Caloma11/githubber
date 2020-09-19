a47184ff6083ba37da6a73395fc2da03a0ccf23b
gianluca perrone <perronegian@hotmail.com>
2020-09-19 11:12:52 +0800

    deleting

--- app/services/github_updater_service.rb
+++ app/services/github_updater_service.rb
@@ -1,15 +1,26 @@
 class GithubUpdaterService
 
-  def create_repos(user, options = {})
-    client = Octokit::Client.new(access_token: user.token)
+  def initialize(user)
+    @user = user
+    @client = Octokit::Client.new(access_token: user.token)
+  end
+
+  def create_repos(options = {})
     number = options[:amount] || 1
     number.times do |n|
-      response = client.create_repository "HubGitterRepo-#{n}", private: true, description: "This is an auto-commiting private repository made with HubGitter"
-      UserRepo.create!(user: user, repo_id: response[:id])
+      response = @client.create_repository "HubGitterRepo-#{n}", private: true, description: "This is an auto-commiting private repository made with HubGitter"
+      UserRepo.create!(user: @user, repo_id: response[:id])
     end
   end
 
-  def auto_commit_for(user)
+  def delete_all_repos
+    @user.user_repos.each do |repo|
+      @client.delete_repository(repo.repo_id.to_i)
+    end
+    @user.user_repos.destroy_all
+  end
+
+  def auto_commit_for
 
   end
 
